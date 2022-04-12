88cb78077ff89f395dca8712fb477901a7ec1d5c
thijsvanloef <thijsvanloef@yoast.com>
2022-04-12 12:35:19 +0200

    add Rick Wouters Fork

--- programs/utils.lua
+++ programs/utils.lua
@@ -1,31 +1,36 @@
-function checkfuel()
-    if turtle.getFuelLevel() == 0 then
+function checkFuel()
+    if turtle.getFuelLevel() < 100 then
         return turtle.refuel(1)
     end
     print(turtle.getFuelLevel())
     return true
 end
- 
+--  location example
+--  top     side
+--   2   ^   5
+--  1T3  |   T
+--   4       6
 local position = 2
  
-function checkstone()
+function checkStone()
     print("checking up" .. tostring(turtle.inspectUp()))
     local success, data = turtle.inspectUp()
     print(data.name)
     print(data.metadata)
-    if turtle.inspectUp() ~= 1 then
-        return 4
-    end
-    if turtle.inspectDown() ~= 1 then
+    local stone = "minecraft:stone"
+    if select(2, turtle.inspectUp()) == stone then
         return 5
     end
-    if inspectPosition(1) ~= 1 then
+    if select(2, turtle.inspectDown()) == stone then
+        return 6
+    end
+    if inspectPosition(1) == stone then
         return 1
     end
-    if inspectPosition(2) ~= 1 then
+    if inspectPosition(2) == stone then
         return 2
     end
-    if inspectPosition(3) ~= 1 then
+    if inspectPosition(3) == stone then
         return 3
     end    
     return -1            
@@ -33,7 +38,7 @@ end
  
 function inspectPosition(pos)
     rotateTo(pos)
-    return turtle.inspect()
+    return select(2, turtle.inspect()).metadata
 end
  
 function rotateTo(pos)
@@ -51,6 +56,32 @@ function rotateTo(pos)
         position=position-1
         rotateTo(pos)
         return
+    end
+end
+
+function isFull()
+    for i = 2,1 do
+        if turtle.getItemSpace(i) > 0 then
+            return false
+        end
+    end
+    return true
+end
+
+function mine(pos)
+    rotateTo(pos)
+    turtle.dig()
+end
+
+function moveForward()
+    mine(2)
+    return turtle.forward()
+end
+
+function returnPaces(nPaces)
+    rotateTo(4)
+    for i = 1, nPaces do
+        turtle.forward()
     end
 end
 
